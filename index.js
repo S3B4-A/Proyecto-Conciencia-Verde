4bb7abd6fde10bea0be82f0cbc6b84bc70c4a464
S3BA <se_hereu@yahoo.com.ar>
2023-10-14 21:04:44 +0000

    subir archvos

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hola Vueasdasd!'
+    }
+  })
+
+
+
+
+// var app3 = new Vue({
+//     el: '#app-3',
+//     data: {
+//       seen: true
+//     }
+//   })
